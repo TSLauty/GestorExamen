281442875fd45c5351ff53bb78b4f7b5c3f06970
TSLauty <cardenalautaro132@gmail.com>
2026-09-18 10:07:15 -0300

    Refactor ResultadoDAL to use database connection object

--- Gestor de Examenes actual/Clases/DAL/ResultadoDAL.php
+++ Gestor de Examenes actual/Clases/DAL/ResultadoDAL.php
@@ -1,46 +1,53 @@
 <?php
-    require_once("Resultado.php");
+require_once __DIR__ . "/../Resultado.php";
+require_once __DIR__ . '/../../Pagina Web/conexion.php';
 
-    class ResultadoDAL {
-        private $usuario = 'root';
-        private $contrasena = '1234';
-        private $servidor = "localhost";
-        private $basededatos = 'gestor_examenes';
-    
-        public function insertResultado($resultado) {
-            $conexion = mysqli_connect($this -> servidor, $this -> usuario, $this -> contrasena) or die ("Error al conectar: ");
-            mysqli_set_charset($conexion, 'utf8');
-            $baseDatos = mysqli_select_db($conexion, $this -> basededatos) or die ("Error seleccionar la BD: ");
+class ResultadoDAL {
 
-            $consulta = (sprintf("INSERT INTO resultados (fechaResultado, calificacion, cantidadErrores, cantidadAciertos, idExamen) VALUES('%s', '%s', '%s', '%s', '%s');",
-            $resultado -> getFechaResultado(), $resultado -> getCAaificacion(), $resultado -> getCantidadErrores(), $resultado -> getCantidadAciertos(), $resultado -> getIdExamen()));
+    private $conexion;
 
-            mysqli_query($conexion, $consulta);
-
-            $idResultado = mysqli_insert_id($conexion);
-            $resultado -> setIdResultado($idResultado);
-            
-            mysqli_close($conexion);
-        }
-
-        public function getResultados(): array {
-            $conexion = mysqli_connect($this -> servidor, $this -> usuario, $this -> contrasena) or die ("Error al conectar: ");
-            mysqli_set_charset($conexion, 'utf8');
-            $baseDatos = mysqli_select_db($conexion, $this -> basededatos) or die ("Error seleccionar la BD: ");
-
-            $consulta = (sprintf("SELECT * FROM resultados"));
-            $resultado = mysqli_query($conexion, $consulta);
-            $registros = array();
-
-            while($registro = mysqli_fetch_array($resultado)) {
-                $resultado = new Resultado ($registro["idResultado"], $registro["fechaResultado"], $registro["calificacion"], $registro["cantidadErrores"], $registro["cantidadAciertos"], $registro["idExamen"]);
-
-                $registros[] = $resultado;
-            } 
-            
-            mysqli_close($conexion);
-
-            return $registros;
-        }
+    public function __construct() {
+        global $conexion;
+        $this->conexion = $conexion;
     }
+
+    public function insertResultado($resultado) {
+        $consulta = sprintf(
+            "INSERT INTO resultados (fechaResultado, calificacion, cantidadErrores, cantidadAciertos, idExamen) VALUES('%s', '%s', '%s', '%s', '%s')",
+            $this->conexion->real_escape_string($resultado->getFechaResultado()),
+            $this->conexion->real_escape_string($resultado->getCalificacion()),
+            $this->conexion->real_escape_string($resultado->getCantidadErrores()),
+            $this->conexion->real_escape_string($resultado->getCantidadAciertos()),
+            $this->conexion->real_escape_string($resultado->getIdExamen())
+        );
+
+        if (!$this->conexion->query($consulta)) {
+            die("Error al insertar resultado: " . $this->conexion->error);
+        }
+
+        $idResultado = $this->conexion->insert_id;
+        $resultado->setIdResultado($idResultado);
+    }
+
+    public function getResultados(): array {
+        $consulta = "SELECT * FROM resultados";
+        $resultado = $this->conexion->query($consulta);
+        $registros = array();
+
+        while ($registro = $resultado->fetch_assoc()) {
+            $resultado = new Resultado(
+                $registro["idResultado"],
+                $registro["fechaResultado"],
+                $registro["calificacion"],
+                $registro["cantidadErrores"],
+                $registro["cantidadAciertos"],
+                $registro["idExamen"]
+            );
+
+            $registros[] = $resultado;
+        }
+
+        return $registros;
+    }
+}
 ?>
