7ae637ce83902c41effae38a453f852f3b041c0e
TSLauty <cardenalautaro132@gmail.com>
2026-09-18 00:01:11 -0300

    Refactor database connection to use environment variables

--- Gestor de Examenes actual/Pagina Web/conexion.php
+++ Gestor de Examenes actual/Pagina Web/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$usuario = 'root';
-$contrasena = '1234';
-$servidor = "localhost";
-$basededatos = 'gestor_examenes';
+$usuario = $_ENV['DB_USER'] ?? getenv('DB_USER') ?? 'root';
+$contrasena = $_ENV['DB_PASSWORD'] ?? getenv('DB_PASSWORD') ?? '';
+$servidor = $_ENV['DB_HOST'] ?? getenv('DB_HOST') ?? 'localhost';
+$basededatos = $_ENV['DB_NAME'] ?? getenv('DB_NAME') ?? 'gestor_examenes';
 $charset = 'utf8mb4';
 
 $conexion = new mysqli($servidor, $usuario, $contrasena, $basededatos);
@@ -12,6 +12,5 @@ if ($conexion->connect_error) {
 }
 
 $conexion->set_charset("utf8");
-
 
 ?>
