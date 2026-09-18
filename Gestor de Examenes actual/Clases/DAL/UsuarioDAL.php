2eea1f93422da8c43fe7ee1d5cc71755dc1a61f8
TSLauty <cardenalautaro132@gmail.com>
2026-09-18 09:41:24 -0300

    Update connection path in UsuarioDAL.php

--- Gestor de Examenes actual/Clases/DAL/UsuarioDAL.php
+++ Gestor de Examenes actual/Clases/DAL/UsuarioDAL.php
@@ -1,6 +1,6 @@
 <?php
     require_once(__DIR__ . "/../Usuario.php");
-require_once __DIR__ . '/../../../conexion.php';
+    require_once __DIR__ . '/../../Pagina Web/conexion.php';
 
     class UsuarioDAL {
      
